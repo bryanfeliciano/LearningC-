f5da0f2ff504511b1353976d15d57010cdea0e6d
bryanfeliciano <71857776+bryanfeliciano@users.noreply.github.com>
2022-06-15 16:13:58 +0000

    intro to data types complete

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
 
+// most basic data types
+  // int age = 25 ;
+  // float weigth = 75.2 ;
+  // double balance = 100.00 ;
+  // char gennder = 'M';
+  // string username = "Bryan" ;
+  // bool isTodaySunny = false ;
+  // string colors[] = {"something", "like", "this"};
+  // cout << colors[0];
+  // cin >> colors[0];
+  // cout << colors[0];
+
 int main() {
-  int age = 25 ;
-  float weigth = 75.2 ;
-  double balance = 100.00 ;
-  char gennder = 'M';
-  string username = "Bryan" ;
-  bool isTodaySunny = false ;
+
 }
