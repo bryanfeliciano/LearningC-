f3f747f511ecb9bfc18202648b7185355238de9b
bryanfeliciano <71857776+bryanfeliciano@users.noreply.github.com>
2022-06-16 23:02:00 +0000

    infinite loops are fun

--- .ccls-cache/@home@runner@LearningC/main.cpp
+++ .ccls-cache/@home@runner@LearningC/main.cpp
@@ -12,7 +12,20 @@ using namespace std;
   // cout << colors[0];
   // cin >> colors[0];
   // cout << colors[0];
-
+// we have to handle memory ourselves and be mindful of a var's byte size(that's weird)
 int main() {
-
+  cout << "WHILE:/n";
+  int counter = 1 ;
+  while(counter<=10){
+    cout << counter << endl;
+  }
 }
+// Instead of this you could use turnanry operators
+  // if (isTodaySunny = true){
+  //   cout << "Go to the park" << endl;
+  // }
+  // else {
+  //   cout << "Stay home" << endl;
+// turnary operator -->
+  // isTodaySunny? cout << "Go to the park": cout << "Take an ummbrella";
+// next we experimented with switch cases
